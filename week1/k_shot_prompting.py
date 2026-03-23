c100480f64b37693920fcd6dda79f4b45ad4a6d0
MyXuehy <xuehy2001@126.com>
2026-03-23 23:36:13 +0800

    feat:k_shot_prompt修改

--- week1/k_shot_prompting.py
+++ week1/k_shot_prompting.py
@@ -7,7 +7,30 @@ load_dotenv()
 NUM_RUNS_TIMES = 5
 
 # TODO: Fill this in!
-YOUR_SYSTEM_PROMPT = ""
+# YOUR_SYSTEM_PROMPT = ""
+YOUR_SYSTEM_PROMPT = """
+You are a deterministic text transformation engine.
+Your only job is to reverse one input word character-by-character.
+
+Output contract:
+- Output exactly one line.
+- Output only the reversed word.
+- No extra text, spaces, punctuation, quotes, markdown, or labels.
+
+Examples:
+Input: apple
+Output: elppa
+
+Input: coding
+Output: gnidoc
+
+Input: llama
+Output: amall
+
+Critical check for this task:
+Input: httpstatus
+Output: sutatsptth
+"""
 
 USER_PROMPT = """
 Reverse the order of letters in the following word. Only output the reversed word, no other text:
